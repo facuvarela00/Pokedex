c52d7bb89f30b97a1a9a6ec1ea54e401d216da49
facuvarela00 <facundo.varela00@gmail.com>
2023-09-20 16:40:27 -0300

    ahorasi

--- Funcionalidades/Informacion/visualizarPokemon.php
+++ Funcionalidades/Informacion/visualizarPokemon.php
@@ -15,17 +15,42 @@
     </header>
     
     <main>
-    <div class="tarjeta">
+
+        <?php
+
+        $id=$_GET["id"];
+        $nombre=$_GET["nombre"];
+        $tipo=$_GET["tipo"];
+        $descripcion=$_GET["descripcion"];
+        $imagen=$_GET["imagen"];
+
+        echo'
+             <div class="tarjeta">
+             <div class="imagenPokemon">';
+        echo'<img src="/Pokedex/Imagenes/Pokemon/'.$imagen.'" alt="Pokemon">';
+        echo'</div>
+             <div class="info">';
+        echo '<h1 class="datos">Nombre:'.$nombre.'</h1>';
+        echo '<h2 class="datos2">'.$id.'</h2>';
+        echo '<h3 class="datos">Tipo: </h3> <h5 class="datos2">'.$tipo.'</h5>';
+        echo ' <h3 class="datos">Descripción: </h3> <h5 class="datos2">'.$descripcion.'.</h5>';
+        echo '</div>
+             </div>';
+        ?>
+
+   <!-- <div class="tarjeta">
         <div class="imagenPokemon">
         <img src="/Pokedex/Pikachu.png" alt="Pokemon">
         </div>
         <div class="info">
+
             <h1 class="datos">Nombre: Pikachu</h1>
             <h2 class="datos2">N°25</h2>
             <h3 class="datos">Tipo: </h3> <h5 class="datos2">Eléctrico</h5>
             <h3 class="datos">Descripción: </h3> <h5 class="datos2">Cuando se enfada, este Pokémon descarga la energía que almacena en el interior de las bolsas de las mejillas.</h5>
         </div>
-    </div>
+    </div>-->
+
     </main>
 
     <footer>
--- Funcionalidades/funciones.php
+++ Funcionalidades/funciones.php
@@ -1,19 +1,21 @@
 <?php
 function mostrarLogged($resultado){
     echo '<section class="registro_pokemon">';
+    echo '<a href="/Pokedex/Funcionalidades/Informacion/visualizarPokemon.php?id='.$resultado["id"].'&nombre='.$resultado["nombre"].'&tipo='.$resultado["tipo"].'&descripcion='.$resultado["descripcion"].'&imagen='.$resultado["imagen"].'">';
     echo '<article class="col_icono_pokemon">';
-    echo '<img class="icono_pokemon_registro" src="Imagenes/Otras/pokeball_registro.png">';
+    echo '<img class="icono_pokemon_registro" src="/Pokedex/Imagenes/Otras/pokeball_registro.png">';
     echo '</article>';
+    echo '</a>';
     echo '<article class="col_data">';
     echo '<h2 class="mostrar_id_pokemon">#'. $resultado["id"].'</h2><br>';
     echo '<h2 class="mostrar_nombre_pokemon">'.$resultado['nombre'].'</h2><br>';
-    echo '<img class="mostrar_tipo_pokemon" src="Imagenes/Tipo/'.$resultado['tipo'].'">';
+    echo '<img class="mostrar_tipo_pokemon" src="/Pokedex/Imagenes/Tipo/'.$resultado['tipo'].'">';
     echo '</article>';
     echo '<article class="col_imagen_poke">';
-    echo '<img class="mostrar_imagen_pokemon" src="Imagenes/Pokemon/'.$resultado['imagen'].'" alt="">';
+    echo '<img class="mostrar_imagen_pokemon" src="/Pokedex/Imagenes/Pokemon/'.$resultado['imagen'].'" alt="">';
     echo '</article>';
     echo '<article class="col_modificaciones">';
-    echo '<a class="imagen_modificar" href="#"></a>';
+    echo '<a class="imagen_modificar" href="/Pokedex/Funcionalidades/Informacion/visualizarPokemon.php?id='.$resultado["id"].'&nombre='.$resultado["nombre"].'&tipo='.$resultado["tipo"].'&descripcion='.$resultado["descripcion"].'&imagen='.$resultado["imagen"].'"></a>';
     echo '<a class="imagen_eliminar" href="#"></a>';
     echo '</article>';
     echo '</section>';
@@ -21,9 +23,11 @@ function mostrarLogged($resultado){
 
 function mostrar($resultado){
     echo '<section class="registro_pokemon">';
+    echo '<a href="/Pokedex/Funcionalidades/Informacion/visualizarPokemon.php?id='.$resultado["id"].'&nombre='.$resultado["nombre"].'&tipo='.$resultado["tipo"].'&descripcion='.$resultado["descripcion"].'&imagen='.$resultado["imagen"].'">';
     echo '<article class="col_icono_pokemon">';
     echo '<img class="icono_pokemon_registro" src="/Pokedex/Imagenes/Otras/pokeball_registro.png">';
     echo '</article>';
+    echo '</a>';
     echo '<article class="col_data">';
     echo '<h2 class="mostrar_id_pokemon">#'.$resultado["id"].'</h2><br>';
     echo '<h2 class="mostrar_nombre_pokemon">'.$resultado['nombre'].'</h2><br>';

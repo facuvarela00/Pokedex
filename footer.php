723cddbec53773c4c3fe80d402ffbba2e295969c
Zogar89 <Zogar89@users.noreply.github.com>
2023-09-23 19:36:01 -0300

    Cami

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Footer</title>
-    <link rel="stylesheet" href="/Pokedex/Estilos/estiloFooter.css">
+    <link rel="stylesheet" href="/Pokedex/Estilos/estilos.css">
 </head>
 <body>
     <footer class="contenedor_footer">        

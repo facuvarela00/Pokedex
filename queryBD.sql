21e319dff772c20c87e18e9fc9f955843966f2da
facuvarela00 <facundo.varela00@gmail.com>
2023-09-25 15:32:03 -0300

    the end

--- queryBD.sql
+++ queryBD.sql
@@ -8,3 +8,8 @@ CREATE TABLE pokemon (
                            descripcion varchar(500),
                            imagen varchar(100)
 );
+
+INSERT INTO pokemon (id, nombre, tipo, descripcion, imagen) 
+VALUES (1, 'Bulbasaur', 'planta.png', 'Este Pokémon nace con una semilla en el lomo, que brota con el paso del tiempo.', '1.png'),
+(2, 'Ivysaur', 'planta.png', 'Cuando le crece bastante el bulbo del lomo, pierde la capacidad de erguirse sobre las patas traseras.', '2.png'),
+(3, 'Venusaur', 'planta.png', 'La planta florece cuando absorbe energía solar, lo cual le obliga a buscar siempre la luz del sol.', '3.png');
